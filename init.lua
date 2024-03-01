0c36a03134caaf6d233236d56f26a759fa0e3d67
= <derFeind87@gmail.com>
2024-03-01 18:40:32 +0100

    remap for () [] quote

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require("NNPR704")
+
--- lua/NNPR704/remap.lua
+++ lua/NNPR704/remap.lua
@@ -1,7 +1,14 @@
 local builtin = require('telescope.builtin')
 
-
 vim.g.mapleader = " "
+-- Autocomplete some chars
+vim.keymap.set("i", "\"", "\"\"<ESC>i")
+vim.keymap.set("i", "(", "()<ESC>i")
+vim.keymap.set("i", "[", "[]<ESC>i")
+
+-- vimsp omnifunc for completion testing lsp server 
+vim.keymap.set("n","<leader>i", vim.lsp.omnifunc)
+
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -21,3 +28,13 @@ vim.keymap.set({'i','v','n'}, '<Up>','<Nop>')
 vim.keymap.set({'i','v','n'}, '<Left>','<Nop>')
 vim.keymap.set({'i','v','n'}, '<Right>','<Nop>')
 vim.keymap.set({'i','v','n'}, '<Down>','<Nop>')
+
+vim.keymap.set({'i','v','n'}, '<leader>l',function ()
+    vim.print("starting lsp server")
+    vim.lsp.set_log_level(2)
+    vim.lsp.start({
+      name = "clb-lsp",
+      cmd = {"C:\\Users\\derFe\\programming\\cbl-lsp\\target\\debug\\cbl-lsp.exe"},
+      root_dir = vim.fs.dirname("C:\\Users\\derFe\\programming\\cbl-lsp\\"),
+    })
+end)
