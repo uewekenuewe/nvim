b12bf3d3899d5754296d81f19af07f0a21142099
= <derFeind87@gmail.com>
2025-08-05 20:22:47 +0200

    basic settings + hotkeys start + lua lsp

--- init.lua
+++ init.lua
@@ -1,1 +1,55 @@
-require("NNPR704")
+vim.g.mapleader = ' '
+-- line numbers
+vim.wo.relativenumber = true
+-- enable clipboard windows
+vim.opt.clipboard = 'unnamedplus'
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+-- indenting
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+-- no swap + SHA files
+vim.opt.swapfile = false
+vim.opt.shadafile = 'NONE'
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+
+-- remaps
+require('remap')
+
+-- plugins
+
+-- lsp
+vim.lsp.config['luals'] = {
+    -- Command and arguments to start the server.
+    cmd = { 'lua-language-server' },
+    -- Filetypes to automatically attach to.
+    filetypes = { 'lua' },
+    -- Sets the 'workspace' to the directory where any of these files is found.
+    -- Files that share a root directory will reuse the LSP server connection.
+    -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+    root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+    -- Specific settings to send to the server. The schema is server-defined.
+    -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            }
+        }
+    }
+}
+
+vim.lsp.enable('luals')
+
+
